219fe35e73bb7680a2f96d0df6aceb942c52b707
Samuel Cruz <dashk1ll@192.168.1.5>
2021-05-09 06:19:35 -0500

    BackEnd completo

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,38 +1,35 @@
 //Importamos los módulos
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const cors = require('cors');
-const midd = require('./midd/midd');
-const serviciosMercadoLibre = require("./services/services");
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const cors = require("cors")
+const middle = require('./midd/midd')
+const serviceAPI = require ('./services/services')
 
-//middleware globales
-app.use(express.json());
-app.use(midd.limiter);
-app.use(cors());
+//Middleware globales
+app.use(express.json())
+app.use(cors())
+app.use(middle.limiter)
 
 //iniciar nuestro servidor
-
 app.listen(process.env.PORT, () => {
-  console.log(
-    `Servidor iniciado en http://${process.env.HOST}:${process.env.PORT}`
-  );
+  console.log(`El servidor esta iniciando en http://${process.env.HOST}:${process.env.PORT}`);
 });
 
 //middleware errores globales
 app.use((err, req, res, next) => {
-  console.log(err);
-  if (!err) {
-    return next();
-  }
+    console.log(err);
+    if (err){
+        return res
+        .status(503)
+        .json("No esta disponible en este momento")
+    }else{
+        return next()
+    }
+})
 
-  return res
-    .status(500)
-    .json("Se produjo un error inesperado, intente nuevamente");
-});
-
-//end point inicial
-app.get("/",cors(midd.corsOptions), async function (req, res) {
+//end point inicial G
+/*app.get("/",cors(midd.corsOptions), async function (req, res) {
   let respuesta = {
     codigo: 200,
     error: false,
@@ -40,21 +37,6 @@ app.get("/",cors(midd.corsOptions), async function (req, res) {
   };
   res.send(respuesta);
 });
-
-app.get("/categorias",cors(midd.corsOptions), async function (req, res) {
-  try {
-    let categorias = await serviciosMercadoLibre.getInfoCategoria();
-    console.log("this is cat", categorias);
-    res.send(categorias);
-  } catch (error) {
-    let errorFinal = {
-      error: error.message,
-      message: "Error inesperado",
-    };
-    res.send(errorFinal);
-  }
-});
-
 app.get("/productos/", cors(midd.corsOptions), async function (req,res){
     try{
         id = "MLM1132";
@@ -69,5 +51,44 @@ app.get("/productos/", cors(midd.corsOptions), async function (req,res){
           };
           res.send(errorFinal);
     }
-});
+});*/
+
+//end point inicial 
+app.get("/",cors(middle.corsOptions), (req, res) => {
+    let respuesta = {
+      codigo: 200,
+      status : "OK", 
+      message: "Inicio de la app :D",
+    }
+    res.json(respuesta);
+  })
   
+  //get a categorías
+  app.get("/categoria",cors(middle.corsOptions),async function (req, res)  {
+    try {
+        let categorias = await serviceAPI.getCategoriaInfo()
+        res.send(categorias)
+    } catch (error) {
+      let finalE = {
+        message: "Ocurrió un error",
+        error: error.message,
+        
+      }
+      res.send(finalE)
+    }
+  })
+
+  app.get("/producto", cors(middle.corsOptions), async function (req,res){
+    try{
+        id = "MLM1051"
+        let producto = await serviceAPI.getProductosInfo(id)
+        res.send(producto)
+    }
+    catch(error){
+        let finalE = {
+            error: error.message,
+            message: "Ocurrió un error",
+          }
+          res.send(finalE)
+    }
+})
--- BackEnd/services/services.js
+++ BackEnd/services/services.js
@@ -1,31 +1,57 @@
+//Importamos y exportamos
 const fetch = require('node-fetch');
-const urlProducto = "https://api.mercadolibre.com/sites/MLM/search?category=";
-async function getCategoriasAPIMerca() {
-    let respuesta = await fetch(process.env.urlCategoria);
-    let data = await respuesta.json();
+module.exports = { getCategoriaInfo,getProductosInfo};
+
+//Declaración de variables
+let urlCategoria = process.env.urlCategoria
+let urlProducto = process.env.urlProducto
+
+//Funciones
+async function getCategoria() {
+    let res = await fetch(urlCategoria);
+    let data = await res.json();
     return data;
 }
-async function getInfoCategoria() {
-    let resultado = await getCategoriasAPIMerca();
+async function getCategoriaInfo() {
+    
+    let resultado = await getCategoria();
+    console.log(resultado)
+    try {
+        if (!resultado){
+            throw new Error("La url esta dañada")
+            
+        }else{
+            console.log("La url es correcta")
+        }
+    }catch (e){
+        throw new Error(e.message)
+        
+    }
     return resultado;
 }
 //getInfoCategoria();
 
-async function getProductosByCategoria(id) {
-    let urlIdProducto = urlProducto + id;
-    let respuesta = await fetch(urlIdProducto);
-    let data = await respuesta.json();
-    return data;
+
+async function getProductCategoria(id) {
+    let urlProduct = urlProducto + id;
+    try{
+        if(!id){
+            throw new Error("No existe un ID")
+        }else{
+            console.log(":D")
+            let res = await fetch(urlProduct);
+            let data = await res.json();
+            return data;
+        }
+    }catch (e){
+        throw new Error (e.message)
+    }
+    
 }
-async function getInfoProductos(id) {
-    let resultado = await getProductosByCategoria(id);
-    console.log(resultado);
+async function getProductosInfo(id) {
+    let resultado = await getProductCategoria(id);
     return resultado;
 }
 
-module.exports = { getInfoCategoria,getInfoProductos};
 
 
-//getInfoProductos("MLM1132");
-
-//getInfoProductos("MLM1403");}
